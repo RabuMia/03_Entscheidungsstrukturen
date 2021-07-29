0806653039c066445ea866148169168d28456c48
RabuMia <Excelchen@hotmail.de>
2021-07-29 12:30:47 +0200

    if-schleife ++

--- script_01.js
+++ script_01.js
@@ -1,2 +1,31 @@
 // Entscheidungsstrukturen | control structures
 
+// Deklaration
+let ageJohn, ageMark;
+let isJohnOlder, isJohnEqual;
+
+// Wertzuweisung
+ageJohn = 35;
+ageMark = 30;
+
+// Test | Logische Aussage
+isJohnOlder = (ageJohn > ageMark);
+isJohnEqual = (ageJohn == ageMark);
+// Ausgabe 
+console.log("isJohnOlder: " + isJohnOlder);
+console.log("isJohnEqual: " + isJohnEqual);
+
+
+/**** IF ****/
+// TINA --> There is no alternative!
+// entweder JA oder nix ... alternativlos
+
+// if (true)
+// if (false) 
+if(isJohnOlder)
+{
+    console.log("John ist älter.");
+}
+
+
+
